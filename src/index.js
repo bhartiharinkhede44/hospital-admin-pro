27ba7372f9b013c3d69fda0c553cc959f423d59e
Bandini2001 <bandinikohare30@gmail.com>
2023-10-02 17:11:10 +0530

    remove render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Ipd from './views/Ipd/Ipd';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <><Ipd /></>
+  <></>
 );
 
