c3c013dc974673a7f04071c4defe14e334813909
Bandini2001 <bandinikohare30@gmail.com>
2023-10-03 08:43:13 +0530

    modified header section

--- src/components/IpdHeader/IpdHeader.js
+++ src/components/IpdHeader/IpdHeader.js
@@ -4,13 +4,13 @@ function IpdHeader(){
     return(
         <>
         <div className="task-container-1">
-        <h3>Sr.No</h3>
-        <h3>Patient <br />ID</h3> 
-        <h3>Patient <br />Name</h3> 
-        <h3>Room</h3>
-        <h3>Bed <br />No</h3>
-        <h3>Remove <br />Patient</h3>
-        <h3>Edit</h3>
+        <h4>Sr.No</h4>
+        <h4>Patient <br />ID</h4> 
+        <h4>Patient <br />Name</h4> 
+        <h4>Room</h4>
+        <h4>Bed <br />No</h4>
+        <h4>Remove <br />Patient</h4>
+        <h4>Edit</h4>
         </div>
         </>
     )
