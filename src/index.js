875378118e2cedcb92d905ccd627b98ceac8f262
Bandini2001 <bandinikohare30@gmail.com>
2023-10-04 16:09:13 +0530

    add Dashboard

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Ipd from "./views/Ipd/Ipd"
+import DashBoard from './views/DashBoard/DashBoard'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <><Ipd /></>
+  <><DashBoard /></>
 );
 
