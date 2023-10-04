79c67a3a6560335ebd64502f6b9376a40e1f7efe
Bandini2001 <bandinikohare30@gmail.com>
2023-10-04 16:13:36 +0530

    remove dash rendring

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import DashBoard from './views/DashBoard/DashBoard'
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <><DashBoard /></>
+  <></>
 );
 
