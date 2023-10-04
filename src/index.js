e9eaf1c36d09a385c37b8350d3a85b109652169d
Bandini2001 <bandinikohare30@gmail.com>
2023-10-04 16:20:00 +0530

    add home

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Ipd from "./views/Ipd/Ipd"
+import Home from './views/Home/Home'
 
 
 
