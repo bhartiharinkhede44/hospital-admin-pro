5d1e5f0b77b0b66100aacb05983b708fbd1f0686
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-05 17:25:25 +0530

    feat : reg patient map from array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Home from './views/Home/Home'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Home from "./views/Home/Home";
+import Registration from "./views/Registration/Registration";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<Home/>
+  <>
+    <Home />
+    <Registration />
+  </>
 );
