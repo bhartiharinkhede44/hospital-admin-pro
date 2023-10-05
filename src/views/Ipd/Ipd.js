98152aa66baaf0d85a2caa9dbc7ba2f49e744c4c
Bandini2001 <bandinikohare30@gmail.com>
2023-10-05 09:11:24 +0530

    addedit feature in ipd page

--- src/views/Ipd/Ipd.js
+++ src/views/Ipd/Ipd.js
@@ -1,19 +1,261 @@
+// import React, { useEffect, useState } from 'react';
+// import { ipdpatientlist } from '../../data/ipdpatientlist';
+// import IpdPatientListCard from './../../components/IpdPatientList/IpdPatientListCard'
+// import IpdHeader from './../../components/IpdHeader/IpdHeader'
+// import "./Ipd.css";
+// import Header from '../../components/Header/Header';
+// import Dashboard from '../../components/Sidebar/Sidebar';
+// import bed from './bed.png'
+// import add from './add.png'
+// import showToast from 'crunchy-toast';
+// import {saveListToLocalStorage} from './../../data/localstorage';
+
+
+// function Ipd() {
+//   const [patients, setPatients] = useState(ipdpatientlist);
+//   const [searchTerm, setSearchTerm] = useState('')
+
+//   const [srNo, setSrNo] = useState('');
+//   const [id, setId] = useState(0);
+//   const [patientName, setPatientName] = useState('');
+//   const [room, setRoom] = useState('');
+//   const [bedNo, setBedNo] = useState('');
+//   const [isEdit, setIsEdit] = useState('');
+
+
+//   useEffect(() => {
+//     const filteredPatients = ipdpatientlist.filter((patient) => {
+//       const name = patient.patientName.toLocaleLowerCase();
+//       const mobile = patient.id.toString();
+//       const query = searchTerm.toLowerCase();
+//       return (name.includes(query) || mobile.includes(query))
+//     })
+
+//     setPatients(filteredPatients);
+//   }, [searchTerm])
+
+//   useEffect(() => {
+//     const list = JSON.parse(localStorage.getItem('taskminder'));
+//     if (list && list.lenght >= 0) {
+//       setPatients(list)
+//     }
+//   }, [])
+
+//   const findIndexByListId = (taskId) => {
+//     let index;
+
+//     patients.forEach((task, i) => {
+//       if (task.id === taskId) {
+//         index = i
+//       }
+//     })
+//     return index;
+//   }
+//   const clearInputFields = () => {
+//     setSrNo('')
+//     setPatientName('');
+//     setRoom('');
+//     setBedNo('');
+//   }
+
+//   const addPatientToList = () => {
+//     const randomId = Math.floor(Math.random() * 10000);
+//     const obj = {
+//       id: randomId,
+//       srNo: srNo,
+//       patientName: patientName,
+//       room: room,
+//       bedNo: bedNo
+//     }
+
+//     const newPatientList = [...patients, obj]
+//     setPatients(newPatientList)
+
+//     clearInputFields()
+
+//     saveListToLocalStorage(newPatientList);
+//     showToast('Patient added succesfully', 'success', 3000);
+//   }
+
+//   const removePatientFromList = (id) => {
+//     const index = findIndexByListId(id);
+//     const tempArray = patients;
+//     tempArray.splice(index, 1);
+
+//     setPatients([...tempArray])
+
+//     saveListToLocalStorage(tempArray)
+//     showToast('Task remove from list succesfully', 'success', 3000);
+//   }
+//   const setListEditable = (id) => {
+//     setIsEdit(true);
+//     setId(id);
+
+//     const currentEditlist = findIndexByListId(id);
+//     setSrNo(currentEditlist.srNo)
+//     setPatients(currentEditlist.patientName);
+//     setRoom(currentEditlist.room);
+//     setBedNo(currentEditlist.bedNo);
+
+//     console.log(currentEditlist)
+//   }
+
+//   const UpdateList = () => {
+
+//     const indexToUpdate = findIndexByListId(id);
+
+//     const tempArray = patients;
+//     tempArray[indexToUpdate] = {
+//       id: id,
+//       srNo: srNo,
+//       patientName: patientName,
+//       room: room,
+//       bedNo: bedNo
+//     }
+//     setPatients([...tempArray])
+//     saveListToLocalStorage(tempArray);
+//     showToast('Task update succesfully', 'success', 3000);
+
+//     setId(-1);
+//     clearInputFields();
+//     setIsEdit(false);
+//   }
+
+//   return (
+//     <>
+//       <div className='d-flex'>
+//         <div><Dashboard /></div>
+//         <div className='ipd-list-div'>
+//           <Header />
+//           <div className='ipd-top-header d-flex'><img src={bed} /><h1 className='heading'>IPD Patient</h1>
+//           </div>
+
+//           <div className='search-div d-flex'>
+
+//             <div>
+//               <p className='search-bar pe-5'>Search Name/ID : &nbsp;<input
+//                 type='text'
+//                 placeholder='Search ID/Name' className='search'
+//                 searchTerm={searchTerm}
+//                 onChange={(e) => { setSearchTerm(e.target.value) }}
+//               /></p></div>
+
+//           </div>
+//           <div className='container-patien'>
+//             <div>
+//               <IpdHeader />
+//             </div>
+//             <div>
+//               {patients.map((patient, index) => {
+//                 const { srNo, id, patientName, room, bedNo } = patient;
+
+//                 return <IpdPatientListCard
+//                   key={index}
+//                   srNo={srNo}
+//                   id={id}
+//                   patientName={patientName}
+//                   room={room}
+//                   bedNo={bedNo}
+//                   removePatientFromList={removePatientFromList}
+//                   setListEditable ={setListEditable}
+
+//                 />
+//               })}
+//             </div>
+//             {
+//               patients.length === 0 ? <span className='pa-center'>Patient is not found!</span> : null
+//             } </div><hr />
+//           <div>
+//             <h1 className='add-btn'><img src={add} />&nbsp; ADD PATIENT</h1>
+//             <div>
+//               <div>
+//                 <h2 className="text-center">
+//                   {isEdit ? `Update ${id}` : 'Add Patient'}
+//                 </h2>
+//                 <div className="add-patient-to-list-container">
+//                   <form>
+
+//                   <input
+//                       type="text"
+//                       value={srNo}
+//                       onChange={(e) => {
+//                         setSrNo(e.target.value)
+//                       }}
+//                       placeholder="Enter Sr No."
+//                       className="task-input"
+//                     />
+//                   <input
+//                       type="text"
+//                       value={patientName}
+//                       onChange={(e) => {
+//                         setPatientName(e.target.value)
+//                       }}
+//                       placeholder="Enter Patient Name"
+//                       className="task-input"
+//                     />
+
+
+//                     <input
+//                       type="text"
+//                       value={room}
+//                       onChange={(e) => {
+//                         setRoom(e.target.value)
+//                       }}
+//                       placeholder="Room"
+//                       className="task-input"
+//                     />
+
+//                     <input
+//                       type="number"
+//                       value={bedNo}
+//                       onChange={(e) => {
+//                         setBedNo(e.target.value)
+//                       }}
+//                       placeholder="Bed No."
+//                       className="task-input"
+//                     />
+//                      {
+//                         isEdit ?
+
+//                           <button className="btn-add-task" type="button" onClick={UpdateList}>Update </button>
+//                           :
+//                           <button className="btn-add-task" type="button" onClick={addPatientToList}>Add Patient</button>
+
+//                       }
+
+
+//                   </form>
+//                 </div>
+//               </div>
+
+//             </div>
+//           </div>
+//         </div>
+
+//       </div>
+
+//     </>
+
+//   );
+// }
+
+// export default Ipd;
+
 import React, { useEffect, useState } from 'react';
 import { ipdpatientlist } from '../../data/ipdpatientlist';
-import IpdPatientListCard from './../../components/IpdPatientList/IpdPatientListCard'
-import IpdHeader from './../../components/IpdHeader/IpdHeader'
+import IpdPatientListCard from './../../components/IpdPatientList/IpdPatientListCard';
+import IpdHeader from './../../components/IpdHeader/IpdHeader';
 import "./Ipd.css";
 import Header from '../../components/Header/Header';
 import Dashboard from '../../components/Sidebar/Sidebar';
-import bed from './bed.png'
-import add from './add.png'
+import bed from './bed.png';
+import add from './add.png';
 import showToast from 'crunchy-toast';
-import {saveListToLocalStorage} from './../../data/localstorage';
-
+import { saveListToLocalStorage } from './../../data/localstorage';
 
 function Ipd() {
-  const [patients, setPatients] = useState(ipdpatientlist);
-  const [searchTerm, setSearchTerm] = useState('')
+  const [patients, setPatients] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const [srNo, setSrNo] = useState('');
   const [id, setId] = useState(0);
@@ -22,43 +264,45 @@ function Ipd() {
   const [bedNo, setBedNo] = useState('');
   const [isEdit, setIsEdit] = useState('');
 
-
   useEffect(() => {
     const filteredPatients = ipdpatientlist.filter((patient) => {
-      const name = patient.patientName.toLocaleLowerCase();
+      const name = patient.patientName.toLowerCase();
       const mobile = patient.id.toString();
       const query = searchTerm.toLowerCase();
-      return (name.includes(query) || mobile.includes(query))
-    })
+      return name.includes(query) || mobile.includes(query);
+    });
 
     setPatients(filteredPatients);
-  }, [searchTerm])
+  }, [searchTerm]);
 
   useEffect(() => {
     const list = JSON.parse(localStorage.getItem('taskminder'));
-    if (list && list.lenght >= 0) {
-      setPatients(list)
+    if (list && list.length >= 0) {
+      setPatients(list);
     }
-  }, [])
+  }, []);
 
-  const findIndexByTaskId = (taskId) => {
+  const findIndexByListId = (taskId) => {
     let index;
 
     patients.forEach((task, i) => {
       if (task.id === taskId) {
-        index = i
+        index = i;
       }
-    })
+    });
     return index;
-  }
+  };
+
   const clearInputFields = () => {
-    setSrNo('')
+    setSrNo('');
     setPatientName('');
     setRoom('');
     setBedNo('');
-  }
+  };
 
   const addPatientToList = () => {
+
+    
     const randomId = Math.floor(Math.random() * 10000);
     const obj = {
       id: randomId,
@@ -66,60 +310,60 @@ function Ipd() {
       patientName: patientName,
       room: room,
       bedNo: bedNo
-    }
+    };
 
-    const newPatientList = [...patients, obj]
-    setPatients(newPatientList)
+    const newPatientList = [...patients, obj];
+    setPatients(newPatientList);
 
-    clearInputFields()
+    clearInputFields();
 
     saveListToLocalStorage(newPatientList);
-    showToast('Patient added succesfully', 'success', 3000);
-  }
+    showToast('Patient added successfully', 'success', 3000);
+  };
 
   const removePatientFromList = (id) => {
-    const index = findIndexByTaskId(id);
-    const tempArray = patients;
+    const index = findIndexByListId(id);
+    const tempArray = patients.slice();
     tempArray.splice(index, 1);
 
-    setPatients([...tempArray])
+    setPatients(tempArray);
 
-    saveListToLocalStorage(tempArray)
-    showToast('Task remove from list succesfully', 'success', 3000);
-  }
+    saveListToLocalStorage(tempArray);
+    showToast('Task removed from list successfully', 'success', 3000);
+  };
+
   const setListEditable = (id) => {
     setIsEdit(true);
     setId(id);
 
-    const currentEditlist = findIndexByTaskId(id);
-    setSrNo(currentEditlist.srNo)
-    setPatients(currentEditlist.patientName);
+    const currentEditlist = findIndexByListId(id);
+    setSrNo(currentEditlist.srNo);
+    setPatientName(currentEditlist.patientName);
     setRoom(currentEditlist.room);
     setBedNo(currentEditlist.bedNo);
 
-    console.log(currentEditlist)
-  }
+    console.log(currentEditlist);
+  };
 
   const UpdateList = () => {
+    const indexToUpdate = findIndexByListId(id);
 
-    const indexToUpdate = findIndexByTaskId(id);
-
-    const tempArray = patients;
+    const tempArray = [...patients];
     tempArray[indexToUpdate] = {
       id: id,
       srNo: srNo,
       patientName: patientName,
       room: room,
       bedNo: bedNo
-    }
-    setPatients([...tempArray])
+    };
+    setPatients(tempArray);
     saveListToLocalStorage(tempArray);
-    showToast('Task update succesfully', 'success', 3000);
+    showToast('Task updated successfully', 'success', 3000);
 
     setId(-1);
     clearInputFields();
     setIsEdit(false);
-  }
+  };
 
   return (
     <>
@@ -127,21 +371,21 @@ function Ipd() {
         <div><Dashboard /></div>
         <div className='ipd-list-div'>
           <Header />
-          <div className='ipd-top-header d-flex'><img src={bed} /><h1 className='heading'>IPD Patient</h1>
-          </div>
+          <div className='ipd-top-header d-flex'><img src={bed} /><h1 className='heading'>IPD Patient</h1></div>
 
           <div className='search-div d-flex'>
-
             <div>
               <p className='search-bar pe-5'>Search Name/ID : &nbsp;<input
                 type='text'
-                placeholder='Search ID/Name' className='search'
-                searchTerm={searchTerm}
+                placeholder='Search ID/Name'
+                className='search'
+                value={searchTerm}
                 onChange={(e) => { setSearchTerm(e.target.value) }}
-              /></p></div>
-
+              /></p>
+            </div>
           </div>
-          <div className='container-patien'>
+
+          <div className='container-patient'>
             <div>
               <IpdHeader />
             </div>
@@ -157,14 +401,15 @@ function Ipd() {
                   room={room}
                   bedNo={bedNo}
                   removePatientFromList={removePatientFromList}
-                  setListEditable ={setListEditable}
-
+                  setListEditable={setListEditable}
                 />
               })}
             </div>
             {
               patients.length === 0 ? <span className='pa-center'>Patient is not found!</span> : null
-            } </div><hr />
+            }
+          </div>
+          <hr />
           <div>
             <h1 className='add-btn'><img src={add} />&nbsp; ADD PATIENT</h1>
             <div>
@@ -174,8 +419,7 @@ function Ipd() {
                 </h2>
                 <div className="add-patient-to-list-container">
                   <form>
-
-                  <input
+                    <input
                       type="text"
                       value={srNo}
                       onChange={(e) => {
@@ -184,7 +428,7 @@ function Ipd() {
                       placeholder="Enter Sr No."
                       className="task-input"
                     />
-                  <input
+                    <input
                       type="text"
                       value={patientName}
                       onChange={(e) => {
@@ -193,8 +437,6 @@ function Ipd() {
                       placeholder="Enter Patient Name"
                       className="task-input"
                     />
-
-
                     <input
                       type="text"
                       value={room}
@@ -204,39 +446,32 @@ function Ipd() {
                       placeholder="Room"
                       className="task-input"
                     />
-
                     <input
                       type="number"
                       value={bedNo}
                       onChange={(e) => {
                         setBedNo(e.target.value)
                       }}
+
                       placeholder="Bed No."
                       className="task-input"
                     />
-                     {
-                        isEdit ?
-
-                          <button className="btn-add-task" type="button" onClick={UpdateList}>Update </button>
-                          :
-                          <button className="btn-add-task" type="button" onClick={addPatientToList}>Add Patient</button>
-
-                      }
-
-
+                    {
+                      isEdit ?
+                        <button className="btn-add-task" type="button" onClick={UpdateList}>Update </button>
+                        :
+                        <button className="btn-add-task" type="button" onClick={addPatientToList}>Add Patient</button>
+                    }
                   </form>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
-
       </div>
-
     </>
-
   );
 }
 
 export default Ipd;
+
